b1491321949195be7f60f0a57fe16277f6efd755
Jack Laxson <jackjrabbit@gmail.com>
2017-10-31 23:58:14 -0400

    add hc command, add travis release stuff

---
+++ src/cmd/healthcheck.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+    "github.com/spf13/cobra"
+    "taskc"
+)
+
+func init() {
+    RootCmd.AddCommand(healthcheckCommand)
+}
+
+var healthcheckCommand = &cobra.Command{
+    Use:   "healthcheck",
+    Short: "run a simple up/down healthcheck",
+    Long:  `Ping the taskd server`,
+    Run: func(cmd *cobra.Command, args []string) {
+        rc := taskc.ReadRC()
+        conn := taskc.Connect(rc)
+        conn.Close()
+    },
+}
